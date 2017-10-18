13a198e7a1ad624f827c063847f0aaf30385db0c
Kateya <dubarry.cynthia@gmail.com>
2017-10-18 13:42:00 +0200

    Ajout gagné

--- jeu.js
+++ jeu.js
@@ -1,20 +1,24 @@
-var nbMystere = Math.floor(Math.random(0,101));
+var nbMystere = Math.floor(Math.random()*100);
 var nbProposition;
 var nbTour = 0;
 var nbTourReste = 10;
 
-function deviner(nbTour) {
+function deviner(tours) {
     while ((nbProposition ==! nbMystere) || (nbTour < 11)) {
-        var nbProposition = prompt('Devinez le nombre compris en 0 et 100 !');
+        var nbProposition = prompt('Devinez le nombre compris en 0 et 100 !'+' Tours restants : ' + nbTourReste);
         if (nbProposition > nbMystere) {
+            alert("C'est moins !")
+            nbTour++;
+            nbTourReste --;
         }
-    
-        nbTour++;
-        nbTourReste --;
+        else if (nbProposition < nbMystere){
+            alert("C'est plus !")
+            nbTour++;
+            nbTourReste --;
+        }
+        else {
+            alert("Gagné ! Le nombre mystère est "+nbMystere);
+        }
+        
     }      
 }
-
-
-
-
-deviner();
