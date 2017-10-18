bec7859b868547d73f35d6d23dacc938330aad49
Kateya <dubarry.cynthia@gmail.com>
2017-10-18 21:11:45 +0200

    ajout verification de l'entrée

--- jeu.js
+++ jeu.js
@@ -1,26 +1,31 @@
 var nbMystere = Math.floor(Math.random()*100);
 var nbProposition;
+var verifNombre = Number.isNaN(nbProposition);
 var nbTour = 0;
 var nbTourReste = 10;
 
 function deviner(tours) {
     for (var i = 1; i<11; i++) {
         var nbProposition = prompt('Devinez le nombre compris en 0 et 100 !'+' Tours restants : ' + nbTourReste);
-        if (nbProposition > nbMystere) {
-            alert("C'est moins !");
-            nbTourReste = (10 - i);
-        }
-        else if (nbProposition < nbMystere){
-            alert("C'est plus !");
-            nbTourReste = (10 - i);
-        }
-        else if (nbProposition == nbMystere) {
-            alert("Gagné ! Le nombre mystère est "+nbMystere);
-            break;
+        if (verifNombre == false) {
+            if (nbProposition > nbMystere) {
+                alert("C'est moins !");
+                nbTourReste = (10 - i);
+            }
+            else if (nbProposition < nbMystere){
+                alert("C'est plus !");
+                nbTourReste = (10 - i);
+            }
+            else if (nbProposition == nbMystere) {
+                alert("Gagné ! Le nombre mystère est "+nbMystere);
+                break;
         }
         else {
-            alert("Tricher, c'est mal !")
+            alert("C'est mal de tricher !");
+            nbTourReste = (10 - i);
+            }
         }
+        
     }        
 }
 
---
+++ play.php
@@ -0,0 +1,1 @@
+<input type="button" value="Jouer !" onclick="deviner(nbTour);" name="" id="">
